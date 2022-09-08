35c6aa377dbcf7fc8f56c32b4eafed2cc7e30bf1
MartyNZ <martyn@solutions-online.co.nz>
2022-09-08 14:10:10 +1200

    9

--- _data/sites/web-connectionz.js
+++ _data/sites/web-connectionz.js
@@ -2,14 +2,14 @@ module.exports = {
 	name: "web-connectionz", // optional, falls back to object key
 	description: "Web Connectionz",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 60 * 10, // (in minutes), 23 hours
     freshChrome: "run"
 	},
 	urls: [
 		"https://martyn.thecooks.net.nz",
     "https://www.solutions-online.co.nz",
-    "https://iportfolio-11ty-sanity.netlify.app",
-    "https://flex-11ty-sanity.netlify.app",
-    "https://nova-11ty-sanity.netlify.app"
+    "https://iportfolio.webconnectionz.com",
+    "https://flex.webconnectionz.com",
+    "https://nova.webconnectionz.com"
 	]
 };
